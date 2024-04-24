bccd442d46b926f994de85e5b3a6b90e08469b59
Albert Okiri <okirialbert@gmail.com>
2024-04-24 11:28:25 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DocsComponent } from './docs/docs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DocsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
